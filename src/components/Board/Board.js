0f11e933b4d2a6db6c88ec07ff35fe13660a51f3
Josue Roque <jroque@applaudstudios.com>
2021-11-02 13:58:39 -0600

    Created resources for Board

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,21 +4,7 @@ import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import "./Board.css";
 import swal from "sweetalert";
-
-const colums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const rows = [
-  { key: "INITIAL", value: ".", number: 0, className: "board-item-initial" },
-  { key: "A", value: "A", number: 1, className: "board-item" },
-  { key: "B", value: "B", number: 2, className: "board-item" },
-  { key: "C", value: "C", number: 3, className: "board-item" },
-  { key: "D", value: "D", number: 4, className: "board-item" },
-  { key: "E", value: "E", number: 5, className: "board-item" },
-  { key: "F", value: "F", number: 6, className: "board-item" },
-  { key: "G", value: "G", number: 7, className: "board-item" },
-  { key: "H", value: "H", number: 8, className: "board-item" },
-  { key: "I", value: "I", number: 9, className: "board-item" },
-  { key: "J", value: "J", number: 10, className: "board-item" },
-];
+import { colums, rows } from "./resources";
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
